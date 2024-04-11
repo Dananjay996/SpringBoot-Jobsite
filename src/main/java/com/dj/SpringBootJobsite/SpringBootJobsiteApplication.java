57e32f8bd514c798aa44cb3a80efe67d2f3a0cce
Dananjay 996 <sreedananjay456@gmail.com>
2024-04-11 18:54:19 +0530

    Project Setup

---
+++ src/main/java/com/dj/SpringBootJobsite/SpringBootJobsiteApplication.java
@@ -0,0 +1,13 @@
+package com.dj.SpringBootJobsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJobsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJobsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dj/SpringBootJobsite/SpringBootJobsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.SpringBootJobsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJobsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
